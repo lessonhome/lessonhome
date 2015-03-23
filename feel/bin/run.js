7538704e838bd35ba0acac9a8e4eea601c92a616
Artem <thepioh@zoho.com>
2015-03-23 11:59:49 +0300

    fixed some buggs

--- feel/bin/run.js
+++ feel/bin/run.js
@@ -7,7 +7,8 @@ module.exports = function(lib){
 
   var Command = require(lib + '/feel/command');
   var command = new Command();
-  command.init().then(command.run).done();
+  //command.init().then(command.run).done();
+  command.run();
 }
 
 
