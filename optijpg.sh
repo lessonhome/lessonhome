fcae8e4cdbbe6e94084b0c785ddcdb0787176124
pioh <thepioh@zoho.com>
2016-02-10 17:23:18 +0300

    optimazed imgs

--- optijpg.sh
+++ optijpg.sh
@@ -1,3 +1,6 @@
 for i in `find . -regex ".*\.\(jpg\|jpeg\)"`
-  do jpegoptim "$i" &
+  do jpegoptim "$i" -s -m70 --all-progressive &
+done
+for i in `find . -regex ".*\.\(gif\|png\)"`
+  do optipng "$i" -o5 -strip all &
 done
